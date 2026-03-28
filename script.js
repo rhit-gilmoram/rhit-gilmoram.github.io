02b82e2fa3d30705df8a29b0c5ff019cc756a987
rhit-gilmoram <gilmoram@rose-hulman.edu>
2026-03-27 20:59:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+//method for not duplicating navbar/footer taken from the following source:
+//https://www.w3tutorials.net/blog/make-header-and-footer-files-to-be-included-in-multiple-html-pages/
+async function loadHTMLElement(elementID, fileName){
+  const response = await fetch(fileName);
+  const htmlText = await response.text();
+  document.getElementById(elementID).innerHTML = htmlText;
+}
 
+loadHTMLElement("navigationBar", "navbar.html");
+loadHTMLElement("footer", "footer.html");
